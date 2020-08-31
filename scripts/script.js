c4c8468f6899bbc40dece0813e112d6efc8b587f
Lucas <lusca-pop@pop-os.localdomain>
2020-08-31 17:06:49 -0300

    add_comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,49 @@
-var page_inclusao, page_alteracao, page_verificacao, info;
+//Criando e Inicializando as variáveis globais.
+var page_inclusao = document.getElementById("page-inclusao"),
+    page_alteracao = document.getElementById("page-alteracao"),
+    page_verificacao = document.getElementById("page-verificacao"),
+    page_blocos = document.getElementById("page-blocos"),
+    btn_expandir = document.getElementById("btn-expandir"),
+    info = document.getElementById("info"),
+    info_update = document.getElementById("info-update"),
+    blockchain = [];
 
-page_inclusao = document.getElementById("page-inclusao");
-page_alteracao = document.getElementById("page-alteracao");
-page_verificacao = document.getElementById("page-verificacao"),
-page_tabelas = document.getElementById("page-tabelas"),
-btn_expandir = document.getElementById("expandir");
+//Inicializando a visualização dos Blocos.
+att_blocos();
 
-info = document.getElementById("info");
-info_update = document.getElementById("info-update");
-
-var blockchain = [];
-
+//Página de Inclusão.
 function inclusao(){
-    att_table();
-    page_tabelas.style = "";
+    //Determinando como a parte dos blocos será mostrada.
+    page_blocos.style = "";
+    page_blocos.className = "col-sm-3 text-center py-3 bg-black";
     btn_expandir.style = "";
-    page_tabelas.className = "col-sm-3 text-center py-3 bg-black";
 
+    //Determinando o que será mostrado na parte principal.
     page_inclusao.style = "";
     page_alteracao.style = "display: none;";
     page_verificacao.style = "display: none;";
 
+    //Determinando o que será mostrado na parte de informações.
     info.innerHTML = "";
     info.className = "";
 
+    //Determinando o que será mostrado nos campos de dados.
     document.getElementById("dificuldade").value = "1";
     document.getElementById("dados").value = "";
+
+    //Atualizando a visualização dos Blocos.
+    att_blocos();
 }
 
+//Processo de Inclusão.
 function incluir(){
-    var block = [],
+    //Criando e inicializando as variáveis.
+    var bloco = [],
         dificuldade = parseInt(document.getElementById("dificuldade").value),
         dados = document.getElementById("dados").value;
 
-    //console.log(typeof(dificuldade));
-
+    //Verificando se cumpre os requisitos para dificuldade e dados.
+    //Caso não cumpra, mostra a mensagem de informação.
     if(dificuldade <= 0 && dados == ""){
         info.innerHTML = "-Selecione uma dificuldade maior que zero e Insira algum dado-";
         info.className = "span-h6 text-light bg-danger";
@@ -48,257 +57,286 @@ function incluir(){
         info.className = "span-h6 text-light bg-danger";
     }
     else{
+        //Criando as variáveis.
+        var mining_values,
+            tempo_antes, 
+            tempo_depois, 
+            tempo;
 
-        block.push(blockchain.length)
+        //Adicionando o número do bloco no bloco.
+        bloco.push(blockchain.length)
 
-        if(blockchain.length == 0){
-            block.push(0x00);
-            //console.log(block);
-        }
-        else{
-            block.push(blockchain[blockchain.length - 1][3]);
-            //console.log(block);
-        }
+        //Verificando se a blockchain está vazia.
+        //Se estiver vazia, o hash do bloco anterior é 0x0.
+        //Se não estiver, adiciona o hash do bloco anterior.
+        if(blockchain.length == 0)
+            bloco.push(0x00);
+        else
+            bloco.push(blockchain[blockchain.length - 1][3]);
     
-        block.push(dados);
+        //Adicionando os dados no bloco.
+        bloco.push(dados);
 
-        var mining_values;
+        //Determinando o tempo antes do processo de mineração.
+        tempo_antes = new Date();
 
-        var time_before, time_after, time;
-        
-        time_before = new Date();
-
+        //Processo de mineração.
         mining_values = mining(dificuldade, dados);
 
-        time_after = new Date();
+        //Determinando o tempo depois do processo de mineração.
+        tempo_depois = new Date();
 
-        time = time_after.getTime() - time_before.getTime();
+        //Calculando quanto tempo durou o processo de mineração.
+        tempo = tempo_depois.getTime() - tempo_antes.getTime();
 
-        console.log("Dif: " + dificuldade + " - T: " + time + "ms");
+        //Mostrando no console a dificuldade e o tempo de duração.
+        console.log("Dif: " + dificuldade + " - T: " + tempo + "ms");
 
-        block.push(mining_values[0]);
+        //Adicionando o valor do hash atual no bloco.
+        bloco.push(mining_values[0]);
     
-        block.push(dificuldade);
+        //Adicionando a dificuldade no bloco.
+        bloco.push(dificuldade);
 
-        block.push(mining_values[1]);
+        //Adicionando o nonce no bloco.
+        bloco.push(mining_values[1]);
 
-        blockchain.push(block);
-    
-        //console.log(blockchain);
+        //Adicionando o bloco na blockchain.
+        blockchain.push(bloco);
 
+        //Mostrando a mensagem de informação.
         info.innerHTML = "-Incluído com sucesso-";
         info.className = "span-h6 text-light bg-success";
 
-        att_table();
+        //Atualizando a visualização dos Blocos.
+        att_blocos();
     }
-
-    //console.log(dificuldade);
-    //console.log(dados);
-
 }
 
+//Página de Alteração.
 function alteracao(bloco){
-    att_table();
-    page_tabelas.style = "";
+    //Determinando como a parte dos blocos será mostrada.
+    page_blocos.style = "";
+    page_blocos.className = "col-sm-3 text-center py-3 bg-black";
     btn_expandir.style = "";
-    page_tabelas.className = "col-sm-3 text-center py-3 bg-black";
 
+    //Determinando o que será mostrado na parte principal.
     page_inclusao.style = "display: none;";
     page_alteracao.style = "";
     page_verificacao.style = "display: none;";
 
+    //Determinando o que será mostrado na parte de informações.
     info_update.innerHTML = "";
     info_update.className = "";
 
+    //Criando e inicializando as variáveis.
     var n_bloco = document.getElementById("n-bloco-update"),
-        hash_anterior_group = document.getElementById("hash-anterior-group"),
         hash_anterior = document.getElementById("hash-anterior"),
+        hash_anterior_group = document.getElementById("hash-anterior-group"),
         dificuldade_update = document.getElementById("dificuldade-update"),
         dados_update = document.getElementById("dados-update");
     
-/*
-    if(typeof(bloco) == 'undefined'){
-        n_bloco.value = '';
-        n_bloco.disabled = false;
-
-        //hash_anterior.value = '';
-        //hash_anterior.disabled = false;
-        hash_anterior_group.style = "display: none;";
-
-        dados_update.value = '';
-    }
-    else{
-*/
-
+    //Determinando o que será mostrado nos campos de dados.
+    //Determinando o campo do número do bloco e desabilita.
     n_bloco.value = blockchain[bloco][0];
     n_bloco.disabled = true;
 
+    //Determinando o campo do hash do bloco anterior e desabilita.
     hash_anterior.value = blockchain[bloco][1];
     hash_anterior.disabled = true;
     hash_anterior_group.style = "";
 
+    //Determinando o campo da dificuldade.
     dificuldade_update.value = blockchain[bloco][4];
 
+    //Determinando o campo de dados.
     dados_update.value = blockchain[bloco][2];
 
-
-//    }
-
+    //Atualizando a visualização dos Blocos.
+    att_blocos();
 }
 
+//Processo de Alteração.
 function alterar(){
+    //Criando e inicializando as variáveis.
     var n_bloco = document.getElementById("n-bloco-update").value,
+        dificuldade_update = parseInt(document.getElementById("dificuldade-update").value),
         dados_update = document.getElementById("dados-update").value;
-        dificuldade_update = parseInt(document.getElementById("dificuldade-update").value);
 
-    if(dados_update == ""){
+    //Verificando se cumpre os requisitos para dificuldade e dados.
+    //Caso não cumpra, mostra a mensagem de informação.
+    if(dificuldade_update <= 0 && dados_update == ""){
+        info_update.innerHTML = "-Selecione uma dificuldade maior que zero e Insira algum dado-";
+        info_update.className = "span-h6 text-light bg-danger";
+    }
+    else if(dificuldade_update <= 0){
+        info_update.innerHTML = "-Selecione uma dificuldade maior que zero-";
+        info_update.className = "span-h6 text-light bg-danger";
+    }
+    else if(dados_update == ""){
         info_update.innerHTML = "-Insira algum dado-";
         info_update.className = "span-h6 text-light bg-danger";
     }
     else{
-        info_update.innerHTML = "-Alterado com sucesso-";
-        info_update.className = "span-h6 text-light bg-success";
-
+        //Criando a variável.
         var mining_values;
 
+        //Processo de mineração.
         mining_values = mining(dificuldade_update, dados_update)
 
+        //Determinando os novos valores do bloco.
         blockchain[n_bloco][2] = dados_update;
         blockchain[n_bloco][3] = mining_values[0];
         blockchain[n_bloco][4] = dificuldade_update;
         blockchain[n_bloco][5] = mining_values[1];
 
-        att_table();
+        //Mostrando a mensagem de informação.
+        info_update.innerHTML = "-Alterado com sucesso-";
+        info_update.className = "span-h6 text-light bg-success";
 
+        //Atualizando a visualização dos Blocos.
+        att_blocos();
     }
 }
 
+//Página e processo de Verificação.
 function verificar(){
-    att_table();
+    //Determinando como a parte dos blocos será mostrada.
+    page_blocos.style = "";
+    page_blocos.className = "col-sm-3 text-center py-3 bg-black";
+    btn_expandir.style = "";
+
+    //Determinando o que será mostrado na parte principal.
     page_inclusao.style = "display: none;";
     page_alteracao.style = "display: none;";
     page_verificacao.style = "";
 
-    page_tabelas.style = "";
-    btn_expandir.style = "";
-    page_tabelas.className = "col-sm-3 text-center py-3 bg-black";
+    //Criando e inicializando as variáveis.
+    var info_integridade = document.getElementById("info-integridade"),
+        bloco = 0;
 
-    var integridade = document.getElementById("integridade");
-
-
-    var block;
-
+    //Realiza a verificação da integridade e determina 
+    //o que será mostrado na parte de informações.
+    //Se a blockchain está vazia ou com somente 1 bloco,
+    //então a integridade é confirmada.
     if(blockchain.length == 0 || blockchain.length == 1){
-        integridade.className = "span-h3 bg-success";
-        integridade.innerHTML = "Integridade Confirmada";
-        //console.log("Right_1")
+        info_integridade.className = "span-h3 bg-success";
+        info_integridade.innerHTML = "Integridade Confirmada";
     }
     else{
-        for(block = 0; block < blockchain.length - 1; block++){
-            if(sha256(blockchain[block][2] + blockchain[block][5]) != blockchain[block + 1][1])
+        //Percorrendo e verificando a blockchain.
+        for(bloco = 0; bloco < blockchain.length - 1; bloco++){
+            if(sha256(blockchain[bloco][2] + blockchain[bloco][5]) != blockchain[bloco + 1][1])
                 break;
         }
 
-        if(block != blockchain.length - 1){
+        //Se não percorreu a blockchain inteira, quer dizer
+        //que a integridade foi violada.
+        if(bloco != blockchain.length - 1){
             var item;
 
-            for(item = blockchain.length - 1; item >= block; item--){
+            //Exclui os blocos após o bloco violado.
+            for(item = blockchain.length - 1; item >= bloco; item--)
                 blockchain.pop();
-            }
 
-            integridade.className = "span-h3 bg-danger";
-            integridade.innerHTML = "Integridade Violada no Bloco " + block + "<br>Todos os blocos após a violação foram exluídos";
-            
-            //console.log("Wrong_1")
-
-            att_table();
-
+            //Mostrando a mensagem de informação.
+            info_integridade.className = "span-h3 bg-danger";
+            info_integridade.innerHTML = "Integridade Violada no Bloco " + bloco + "<br>Todos os blocos após a violação foram exluídos";
         }
 
+        //Mostrando a mensagem de informação.
         else{
-            integridade.className = "span-h3 bg-success";
-            integridade.innerHTML = "Integridade Confirmada";
-            //console.log("Right_2")
+            info_integridade.className = "span-h3 bg-success";
+            info_integridade.innerHTML = "Integridade Confirmada";
         }
     }
 
-
+    //Atualizando a visualização dos Blocos.
+    att_blocos();
 }
 
+//Processo de Limpeza.
 function limpar(){
-    att_table();
-    page_tabelas.style = "display: none;";
+    //Determinando como a parte dos blocos será mostrada.
+    page_blocos.style = "display: none;";
+    page_blocos.className = "col-sm-12 text-center py-3 bg-black";
     btn_expandir.style = "display: none;";
-    page_tabelas.className = "col-sm-12 text-center py-3 bg-black";
 
+    //Determinando o que será mostrado na parte principal.
     page_inclusao.style = "display: none;";
     page_alteracao.style = "display: none;";
     page_verificacao.style = "display: none;";
 
-    var tam,
-        block;
+    //Criando e inicializando as variáveis.
+    var tam = blockchain.length,
+        bloco = 0;
 
-    tam = blockchain.length;
-
-    for(block = 0; block < tam; block++){
+    //Exclui todos os elementos da blockchain.
+    for(bloco = 0; bloco < tam; bloco++){
         blockchain.pop();
     }
-    att_table();
+
+    //Atualizando a visualização dos Blocos.
+    att_blocos();
 }
 
+//Processo de Expansão dos Blocos.
 function expandir(){
-    att_table();
-    page_tabelas.style = "";
+    //Determinando como a parte dos blocos será mostrada.
+    page_blocos.style = "";
+    page_blocos.className = "col-sm-12 text-center py-3 bg-black";
     btn_expandir.style = "display: none;";
-    page_tabelas.className = "col-sm-12 text-center py-3 bg-black";
 
+    //Determinando o que será mostrado na parte principal.
     page_inclusao.style = "display: none;";
     page_alteracao.style = "display: none;";
     page_verificacao.style = "display: none;";
-    att_table();
+
+    //Atualizando a visualização dos Blocos.
+    att_blocos();
 }
 
+//Processo de Mineração.
 function mining(dificuldade, dados){
+    //Criando e inicializando as variáveis.
     var nonce = 0,
         res = '';
 
+    //Encontrando um hash que cumpre os requisitos.
     while(true){
+        //Gerando o hash dos dados com o nonce;
         res = sha256(dados + nonce);
 
+        //Verificando se o hash gerado cumpre os requisitos de dificuldade,
+        //Se cumprir, retorna o hash e o nonce.
         if((res.substring(0, dificuldade).match(/0/g) || []).length == dificuldade){
             return [res, nonce];
         }
 
+        //Incrementando o nonce.
         nonce++;
     }
 }
 
-att_table();
+//Processo de Atualização da Visualização dos Blocos.
+function att_blocos(){
+    //Criando e inicializando as variáveis.
+    var tabelas = "",
+        bloco = 0;
 
-function att_table(){
+    //Criando e inicializando as variáveis que formarão o html.
+    var part1 = "<button onclick=\"alteracao(",
+        part3 = ")\" class=\"btn-table mb-3 p-0 bg-transparent\"><table style=\"word-wrap: break-word; table-layout:fixed;\" class=\"table text-light mb-0\"><thead class=\"thead-light\"><tr><th class=\"py-0 fs-3\">Bloco ",
+        part5 = "</th></tr></thead><tbody><tr class=\"bg-dark\"><th class=\"py-1 fs-3\">Hash do Bloco Anterior</th></tr><tr class=\"bg-secondary\"><td class=\"py-1 fs-3\">",
+        part7 = "</td></tr><tr class=\"bg-dark\"><th class=\"py-1 fs-3\">Dados</th></tr><tr class=\"bg-secondary\"><td class=\"py-1 fs-3\">",
+        part9 = "</td></tr></tbody></table></button>";
 
-    //var part1 = "<a href=\"!#\" style=\"color: transparent\"><table style=\"word-wrap: break-word; table-layout:fixed;\" class=\"table text-light\"><thead class=\"thead-light\"><tr><th class=\"py-0 fs-3\">Bloco ";
-    var part1 = "<button onclick=\"alteracao(";
-
-    var part3 = ")\" class=\"btn-table mb-3 p-0 bg-transparent\"><table style=\"word-wrap: break-word; table-layout:fixed;\" class=\"table text-light mb-0\"><thead class=\"thead-light\"><tr><th class=\"py-0 fs-3\">Bloco ";
-    //n_bloco
-    var part5 = "</th></tr></thead><tbody><tr class=\"bg-dark\"><th class=\"py-1 fs-3\">Hash do Bloco Anterior</th></tr><tr class=\"bg-secondary\"><td class=\"py-1 fs-3\">";
-    //hash_antigo
-    var part7 = "</td></tr><tr class=\"bg-dark\"><th class=\"py-1 fs-3\">Dados</th></tr><tr class=\"bg-secondary\"><td class=\"py-1 fs-3\">";
-    //dados
-    var part9 = "</td></tr></tbody></table></button>"
-
-    var tabela = "",
-        block = 0;
-
-    for(block = 0; block < blockchain.length; block++){
-        tabela += part1 + blockchain[block][0]  + part3 + block + part5 + blockchain[block][1] + part7 + blockchain[block][2] + part9;
+    //Montando o html dos blocos.
+    for(bloco = 0; bloco < blockchain.length; bloco++){
+        tabelas += part1 + blockchain[bloco][0]  + part3 + bloco + part5 + blockchain[bloco][1] + part7 + blockchain[bloco][2] + part9;
     }
 
-    document.getElementById("tabela-lateral").innerHTML = tabela;
+    //Mostrando os blocos na tela.
+    document.getElementById("table-lateral").innerHTML = tabelas;
 }
-
-//[0 - n_bloco][1 - hash_anterior][2 - dados][3 - hash_atual][4 - dificuldade][5 - nonce]
-
-//Alterar algo quando coloca mouse na tabela
 
